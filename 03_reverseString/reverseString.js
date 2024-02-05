959715d8a99613fc7027ba8fe97b24d906ce23fd
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-02-05 18:30:00 +0100

    Update exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	let newArray = str.split("").reverse();
 
+	let newStr = "";
+
+	for (let i = 0; i < newArray.length; i++) {
+		newStr += newArray[i];
+	}
+
+	return newStr;
 };
 
 // Do not edit below this line
