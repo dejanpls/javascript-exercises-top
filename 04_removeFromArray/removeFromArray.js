cf7084ec837180047dfcaf8c342aff63c02d2527
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-02-05 19:07:18 +0100

    Implement a function that takes an array and some arguments then removes the other arguments from that array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(toImport, ...args) {
+	const forDeletion = [];
 
+	for (let arg of args) forDeletion.push(arg);
+
+	const arr = toImport.filter(item => !forDeletion.includes(item))
+
+	return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
