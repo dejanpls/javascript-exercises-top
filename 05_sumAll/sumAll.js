bbb9d82013dfc1d3dbccad7cbda3d6ce8d609618
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-02-06 19:27:44 +0100

    Implement a function that takes 2 integers and returns the sum of every number between(and including) them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+	let sum = 0;
+	let operator1 = 0;
+	let operator2 = 0;
 
+	if (typeof(num1) !== "number" || typeof(num2) !== "number") return "ERROR"
+	else if (num1 < 0 || num2 < 0) return "ERROR"
+
+	if (num1 > num2) {
+		operator1 = num2;
+		operator2 = num1;
+	} else {
+		operator1 = num1;
+		operator2 = num2;
+	}
+
+	for (let i = operator1; i <= operator2; i++) {
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
