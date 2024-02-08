f6c27d67bcba2acda53ae6841e652c4fae7ff615
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-02-08 19:04:23 +0100

    Remove result variable and return celsius and fahrenheit rounded on with one line

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(fahrenheit) {
-    return Math.round(((fahrenheit - 32) * (5 / 9)) * 10) / 10;
+    return Math.round( ( (fahrenheit - 32) * (5 / 9) ) * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
-    return Math.round(((9 / 5 * celsius) + 32) * 10) / 10;  
+    return Math.round( ( (9 / 5 * celsius) + 32) * 10) / 10;  
 };
 
 // Do not edit below this line
