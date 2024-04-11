83963e8292520dc7341f9f8f059adc57b6fd1b35
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-04-11 09:20:08 +0200

    Create a function that returns a specific member of the Fibonacci sequence

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+ 	const str = string
+		 				.toLowerCase()
+		 				.match(/\b[\w]+\b/g) // remove punctuations using regEx
+		 				.join("")
+	const strReversed = str.split("").reverse().join("");
+	return str == strReversed;
 };
 
 // Do not edit below this line
