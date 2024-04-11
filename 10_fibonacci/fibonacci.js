e3d4bb5d2c5609a6507b54fa74035d8c6ae525f3
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-04-11 09:21:34 +0200

    Create a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+	const number = parseInt(value);
+	arr = [];
 
+	if (number < 0) return "OOPS";
+	else {
+		for (let i = 0; i <= number; i++) {
+			if (i == 0) arr.push(i);
+			if (i == 1 || i == 2) arr.splice(0, 0, 1);
+			if (i > 1) {
+				arr.splice(0, 0, (arr[0] + arr[1]));
+			}
+		}
+	}
+
+	return arr.reverse()[number];
 };
 
 // Do not edit below this line
